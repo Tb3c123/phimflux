2c8f563f521c0bc21f882ee306ba80e9dee24e90
hieunth1@hdbank.com.vn <157444237+Tb3c123@users.noreply.github.com>
2026-08-11 15:23:00 +0700

    Fix Android native video player using webview_flutter embed engine

--- lib/core/player/video_player_wrapper.dart
+++ lib/core/player/video_player_wrapper.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import '../theme/app_colors.dart';
 import 'web_iframe/web_iframe_helper.dart';
 
-/// Core Video Player wrapper supporting Web HTML iframe & native stream playback
+/// Core Video Player wrapper supporting Web HTML iframe & Native WebView/ExoPlayer stream playback
 class VideoPlayerWrapper extends StatefulWidget {
   final String videoUrl;
   final String? embedUrl;
@@ -25,7 +26,9 @@ class VideoPlayerWrapper extends StatefulWidget {
 
 class _VideoPlayerWrapperState extends State<VideoPlayerWrapper> {
   VideoPlayerController? _controller;
+  WebViewController? _webViewController;
   bool _hasError = false;
+  bool _useWebView = false;
   late String _viewId;
 
   @override
@@ -37,8 +40,27 @@ class _VideoPlayerWrapperState extends State<VideoPlayerWrapper> {
     _viewId = 'phimflux_iframe_${activeUrl.hashCode}';
 
     if (!kIsWeb) {
-      _initNativePlayer();
+      if (activeUrl.contains('embed') || activeUrl.contains('player') || !activeUrl.endsWith('.m3u8')) {
+        _initWebViewPlayer(activeUrl);
+      } else {
+        _initNativePlayer();
+      }
     }
+  }
+
+  void _initWebViewPlayer(String url) {
+    setState(() => _useWebView = true);
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.black)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onWebResourceError: (error) {
+            debugPrint('WebView Error: ${error.description}');
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(url));
   }
 
   Future<void> _initNativePlayer() async {
@@ -54,9 +76,10 @@ class _VideoPlayerWrapperState extends State<VideoPlayerWrapper> {
       }
       if (mounted) setState(() {});
     } catch (e) {
-      if (mounted) {
-        setState(() => _hasError = true);
-      }
+      final activeUrl = (widget.embedUrl != null && widget.embedUrl!.isNotEmpty)
+          ? widget.embedUrl!
+          : widget.videoUrl;
+      _initWebViewPlayer(activeUrl);
     }
   }
 
@@ -77,6 +100,15 @@ class _VideoPlayerWrapperState extends State<VideoPlayerWrapper> {
         width: double.infinity,
         height: double.infinity,
         child: buildWebIframe(activeUrl, _viewId),
+      );
+    }
+
+    if (_useWebView && _webViewController != null) {
+      return Container(
+        color: Colors.black,
+        width: double.infinity,
+        height: double.infinity,
+        child: WebViewWidget(controller: _webViewController!),
       );
     }
 
